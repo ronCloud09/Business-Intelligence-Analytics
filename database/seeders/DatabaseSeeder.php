760334bea8cbd220779df3167889f4c59bf18eab
Ron <ronrashley.bartolome@cvsu.edu.ph>
2026-07-13 16:33:07 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
         User::factory()->updateOrCreate(
             ['email' => 'test@example.com'],
             ['name' => 'Test User']
