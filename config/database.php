42d2bc8198a2b8adc7c9bb577a385233720f30a3
ronCloud09 <ronrashley.bartolome@cvsu.edu.ph>
2026-07-14 13:36:05 +0800

    added database url support for postgre

--- config/database.php
+++ config/database.php
@@ -34,6 +34,22 @@ return [
 
     'connections' => [
 
+        '' => '',
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'require',
+        ],
+        '' => env('', ''),
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
@@ -151,7 +167,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
